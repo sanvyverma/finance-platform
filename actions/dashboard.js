06bf3c1f419f54885be38710134f0fe1fd03a938
Sanvy Verma <sanvyverma@gmail.com>
2025-06-07 22:29:59 +0530

    Fixes

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -1,6 +1,6 @@
 "use server";
 
-import aj from "@/lib/arcjet";
+import aj from "@/lib/inggest/arcjet";
 import { db } from "@/lib/prisma";
 import { request } from "@arcjet/next";
 import { auth } from "@clerk/nextjs/server";
